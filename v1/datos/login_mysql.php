ac3054d310908d670ab7566abd668d0ae5d3d8bf
darcknico <nicolasrl2005@gmail.com>
2016-09-04 20:03:47 -0300

    empezar

---
+++ v1/datos/login_mysql.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Provee las constantes para conectar a la base de datos Mysql.
+ *
+ */
+
+define("NOMBRE_HOST", "mysql.hostinger.com.ar"); //nombre del host
+define("BASE_DE_DATOS", "u488125577_repo"); //nombre de la base de datos
+define("USUARIO", "u488125577_root"); // nombre del usuario
+define("CONTRASENIA", "123456"); // contrasenia
